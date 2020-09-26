bf002ed41234316118a643cd9f455ea8b3c30ed5
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-26 17:14:25 +0900

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+
   has_secure_token
   has_secure_password
 end
