faeeeaf13f13cfb63b5fff80053ce12c9fc2376c
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-26 17:34:08 +0900

    factory_bot

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |i| "example#{i}i@example.com" }
+    sequence(:password) { |i| "password#{i}" }
+    sequence(:password_confirmation) { |i| "password#{i}" }
   end
 end
