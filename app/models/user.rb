d2521a0434226f2d17fec245350a21ac853cc1b8
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-28 15:58:12 +0900

    メールアドレスに一意の検証追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_token
