213e01f09177429ed71ec096a5fda6e164fad4f4
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-28 15:58:45 +0900

    トークン認証処理追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      User.exists?(token: token)
+    end
+  end
 end
