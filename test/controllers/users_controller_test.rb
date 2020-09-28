74e77207c9cffe5137ac235685ed92d2e82bd890
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-28 16:22:28 +0900

    コントローラテスト修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,35 +2,50 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @new_user = FactoryBot.build(:user)
+    @user = FactoryBot.create(:user)
   end
 
   test "should get index" do
-    get users_url, as: :json
+    get users_url, as: :json, headers: { "Authorization": "Token #{@user.token}" }
     assert_response :success
   end
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, password_digest: @user.password_digest, token: @user.token } }, as: :json
+      post users_url, params: {
+        user: {
+          email: @new_user.email,
+          password: @new_user.password,
+          password_confirmation: @new_user.password_confirmation
+        }
+      }, as: :json
     end
 
     assert_response 201
   end
 
   test "should show user" do
-    get user_url(@user), as: :json
+    get user_url(@user), as: :json, headers: { "Authorization": "Token #{@user.token}" }
     assert_response :success
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password_digest: @user.password_digest, token: @user.token } }, as: :json
+    patch user_url(@user), params: {
+      user: {
+        email: @user.email,
+        password: @user.password,
+        password_confirmation: @user.password_confirmation
+      }
+    },
+    as: :json,
+    headers: { "Authorization": "Token #{@user.token}" }
     assert_response 200
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete user_url(@user), as: :json
+      delete user_url(@user), as: :json, headers: { "Authorization": "Token #{@user.token}" }
     end
 
     assert_response 204
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:email) { |i| "example#{i}i@example.com" }
     sequence(:password) { |i| "password#{i}" }
     sequence(:password_confirmation) { |i| "password#{i}" }
+    token { "abcde" }
   end
 end
