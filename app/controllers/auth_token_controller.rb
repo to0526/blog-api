b1dc489f8fced3385307b30924a9672a77fa086f
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-29 10:00:32 +0900

    ログイン処理

---
+++ app/controllers/auth_token_controller.rb
@@ -0,0 +1,18 @@
+class AuthTokenController < ApplicationController
+  skip_before_action :authenticate, only: [:create]
+
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
+      render plain: user.token
+    else
+      render status: :unauthorized
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+end
